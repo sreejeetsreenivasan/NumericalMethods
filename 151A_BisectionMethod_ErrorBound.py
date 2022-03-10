a02998c220206ef60b063c74cee5cd17c0433150
sreejeetsreenivasan <67654206+sreejeetsreenivasan@users.noreply.github.com>
2022-03-10 12:59:49 -0800

    Add files via upload

---
+++ 151A_BisectionMethod_ErrorBound.py
@@ -0,0 +1,35 @@
+import math
+
+
+def func_expression(x):
+    """
+    :param x: Input into function
+    :return: The function value at x
+    Change the expression for 'solution' based on the function needing to be approximated
+    """
+    solution = x ** 2 - 3
+    return solution
+
+
+approx = []
+error = math.pow(10, -3)
+
+
+def bisection(a, b):
+    global approx
+    mid = (a + b) / 2
+    if len(approx) >= 2:
+        if abs((approx[-1] - approx[-2])) < error:
+            print(f'The Bisection Method requires {len(approx)} iterations to approximate the root within {error}')
+            for i in range(len(approx)):
+                print(f'P{i + 1}: {approx[i]}')
+            return
+    if (func_expression(a) * func_expression(mid)) < 0:
+        approx.append(mid)
+        bisection(a, mid)
+    else:
+        approx.append(mid)
+        bisection(mid, b)
+
+
+bisection(0, 4)
