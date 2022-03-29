052b27fa3a504a763273065ccb3b875e7cf377de
sreejeetsreenivasan <67654206+sreejeetsreenivasan@users.noreply.github.com>
2022-03-28 17:29:59 -0700

    Updated header docstring

--- 151A_CompositeMidpointRule.py
+++ 151A_CompositeMidpointRule.py
@@ -1,5 +1,8 @@
 """
-The following program uses the numerical method of the composite midpoint rule to approximate the area under a curve"
+The following program uses the numerical method of the composite midpoint rule to approximate the area under a curve
+We use the formula that the area under a function f(x) within the interval [a, b] is equal to
+2h * (∑ f(x_2j-1), 1 ≤ j ≤ n/2, where h is the length of each sub-interval (h = (a - b) / n)
+and there exists 'n' such intervals
 """
 
 import math
